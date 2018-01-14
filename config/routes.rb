cca24132e9a626c3b42dffc82cc867b33d85e4e6
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-14 16:12:54 -0500

    initial sessions controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
   resources :categories
-  resources :items
+  resources :items do
+    resources :images
+  end
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
