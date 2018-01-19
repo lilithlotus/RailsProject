663ead2a677117eacf2bdb318c01a3fe4e2bd315
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-19 12:37:10 -0500

    new and create actions for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,20 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:user).permit(:name, :value, :description)
 end
