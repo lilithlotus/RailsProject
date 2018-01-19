3fcd2b0e5bc3af48ed48f6f748aabd05a5da1663
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-19 18:11:22 -0500

    started item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,10 @@ class Item < ApplicationRecord
   belong_to :user
   belongs_to :category
   has_many :images
-  validates :user_id, :name, :category_id, presence: true
+  validates :user_id, :name, :category_name, presence: true
+
+  def category_attributes=(category)
+    self.category = Category.find_or_create_by(name: category.name)
+    self.category.update(category)
+  end
 end
